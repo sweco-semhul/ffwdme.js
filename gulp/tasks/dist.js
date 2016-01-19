be04e24a0cc141bc600ae920c9abefbd1ac8ada9
semhul <martin.hultman@sweco.se>
2016-01-19 13:32:13 +0100

    adding debug css to dist

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -4,5 +4,6 @@ var gulp = require('gulp');
 gulp.task('dist', ['build'], function() {
   gulp.src('./build/ffwdme*').pipe(gulp.dest('./dist'));
   gulp.src('./build/components/**').pipe(gulp.dest('./dist/components'));
+  gulp.src('./build/debug/**').pipe(gulp.dest('./dist/debug'));
 
 });
