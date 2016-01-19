0a26092b65a653b04e40a976d09a9cf56a0a02b2
semhul <martin.hultman@sweco.se>
2016-01-19 13:20:25 +0100

    Adding component assets to dist

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -3,4 +3,6 @@ var gulp = require('gulp');
 
 gulp.task('dist', ['build'], function() {
   gulp.src('./build/ffwdme*').pipe(gulp.dest('./dist'));
+  gulp.src('./build/components/**').pipe(gulp.dest('./dist/components'));
+
 });
